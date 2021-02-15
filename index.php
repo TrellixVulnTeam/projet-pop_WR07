cc27e9f9e90daabce775993813523c70c9f04044
PaulGeneve <geneve.paul1295@gmail.com>
2021-02-15 15:15:37 +0100

    the first commit

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php require 'php/header.php'; ?>
+
+    <main>
+    </main>
+
+<?php require 'php/footer.php'; ?>
---
+++ php/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en,fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>--POP.--</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Jost&family=Merienda+One&display=swap" rel="stylesheet">
+    <link rel="icon" href="" />
+    <link rel="stylesheet" href="style/style.css">
+    <link href="/your-path-to-fontawesome/css/all.css" rel="stylesheet">
+</head>
+
+<body>
+    <header>
+        <div id="header">
+            <div class="nav-gauche">
+                <ahref="#"><img id="logo" src="style/assets/popLogo.png" alt="logoPOP"></a>
+            </div>
+            <div class="nav-droite">
+                <div class="dropdown">
+                    <img class="btn_catégorie" id="photoProfil"src="style/assets/tenjiro1.jpg" alt="image profil">
+                    <!--<button class="btn_catégorie">profil</button>-->
+                    <div class="dropdown-child">
+                        <a class="child" href="#posts">post/activité</a>
+                        <a class="child" href="#aide">aide</a>
+                        <a class="child" href="#parametre">parametre</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
